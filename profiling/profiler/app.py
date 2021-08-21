c09acb543049d073af9e0940eb481c63d44f5e3d
Zhaobo <zzhang1@futurewei.com>
2021-08-21 14:41:50 -0700

    add first pod annotation in profiler, and clean up prometheus scrape config for cadvisor

--- profiling/profiler/app.py
+++ profiling/profiler/app.py
@@ -7,11 +7,13 @@ import logging
 from kubernetes import config, client
 from scipy.stats import norm
 import numpy as np
+import subprocess
+import copy
 
 MEM_UTIL = "DCGM_FI_DEV_MEM_COPY_UTIL"
 GPU_UTIL = "DCGM_FI_DEV_GPU_UTIL"
 DOMAIN = "ai.centaurus.io"
-
+POD_KEY1 = DOMAIN + "/gpu-memused"
 def cyclic_pattern_detection(time_series):
     """input pandas series, detect cyclic pattern return True/False
     if True, return frequency, if false, frequency is -1
@@ -32,17 +34,25 @@ def cyclic_pattern_detection(time_series):
     else:
         return False, -1
 
-def patch_annotation(node_name, ann):
+def patch_annotation(name, ann, namespace=""):
     if 'KUBERNETES_PORT' in os.environ:
         config.load_incluster_config()
     else:
         logging.error("RUNNING cluster is not avaliable")
         exit(1)
+    ann = copy.deepcopy(ann) # copy and then make changes
+    # reformat dictionary, in case it is nested, flatten to one level by cast nested dict to string
+    for k, v in ann.items():
+        if v is not None:  # if v is None, it means delete the existing annotaion
+            ann[k]=str(v)
+
     body = {'metadata': {'annotations':ann}}
 
     v1 = client.CoreV1Api()
-    logging.info("Initally add gpu static attributes \n{}".format(body))
-    v1.patch_node(node_name, body)
+    if namespace == "": # patch node
+        v1.patch_node(name, body)
+    else: # patch pod
+        v1.patch_namespaced_pod(name, namespace, body)
     return True
 
 def collect_cur_usage(gpu_idx):
@@ -57,14 +67,69 @@ def collect_cur_usage(gpu_idx):
         cur_usage['pid-mem'] = [(i.pid, str(math.ceil(i.usedGpuMemory/pow(1024,2)))+'MB') for i in processes]
     return cur_usage
 
-def profiling(url, pod_ip, ana_window='2m', metrics=MEM_UTIL):
+
+def init_remove_annotation(node_name, ann_key):
+    if 'KUBERNETES_PORT' in os.environ:
+        config.load_incluster_config()
+    else:
+        logging.error("RUNNING cluster is not avaliable")
+        exit(1)
+    #body = {'metadata': {'annotations':ann}}
+    v1 = client.CoreV1Api()
+    # 1) get pod name and namespace on the node
+    field_selector = 'spec.nodeName='+node_name
+    ret = v1.list_pod_for_all_namespaces(watch=False, field_selector=field_selector)
+    for i in ret.items:
+        if i.metadata.annotations is not None and POD_KEY1 in i.metadata.annotations:
+            patch_annotation(i.metadata.name, {POD_KEY1:None}, i.metadata.namespace) 
+            logging.info("Init reset pod {}'s annotation{}".format(i.metadata.name,i.metadata.annotations[POD_KEY1]))
+
+def collect_pod_metrics(pid_mem_array, node_name, gpu_id, pods_ann):
+    """
+    pods_ann: dict()
+    key: dlt-job:default  #podname:namespace
+    value (annotations): {"ai.centaurusinfra.io/gpu-memused":[(0,137MB),(1,1125MB)]}, #tuple (gpu_id, memused) 
+    """
+    if 'KUBERNETES_PORT' in os.environ:
+        config.load_incluster_config()
+    else:
+        logging.error("RUNNING cluster is not avaliable")
+        exit(1)
+    v1 = client.CoreV1Api()
+    # 1) get pod name and namespace on the node
+    field_selector = 'spec.nodeName='+node_name
+    ret = v1.list_pod_for_all_namespaces(watch=False, field_selector=field_selector)
+    pod_ns = dict()
+    for i in ret.items:
+        pod_ns[i.metadata.name] = i.metadata.namespace
+    # 2) get pod name by pid
+    bashCmd = ["nsenter", "--target", "XXX", "--uts", "hostname"]
+    for pid, mem_used in pid_mem_array:
+        bashCmd[2]=str(pid)
+        subp = subprocess.Popen(bashCmd, stdout=subprocess.PIPE)
+        output, error = subp.communicate()
+        if error is None:
+            pod_name = output.decode("utf-8").rstrip()
+            # 3) format results to dict, key: "podname:namespace:gpu_id", value:"{DOMAIN + "/gpu-memused":memused}"
+            key = pod_name + ":" + pod_ns[pod_name]
+            if key in pods_ann:
+                pods_ann[key][POD_KEY1].append((gpu_id,mem_used)) # append other gpu used by this pod
+            else:
+                value = {POD_KEY1:[(gpu_id,mem_used)]}
+                pods_ann[key] = value
+        else:
+            logging.error("nsenter failed to acquire pod name,{}".format(error))
+    return pods_ann
+
+def profiling(url, pod_ip, node_name, ana_window='2m', metrics=MEM_UTIL):
     """if key exists, the value will be replaced,
        add dynamic status
        {ai.centaurus.io/gpu0:{cur_mem_used:4GB, max_gpu_util:60, max_mem_cpy_util:34, cyclic:True, process_cnt:1},
         ai.centaurus.io/gpu1:{cur_mem_used:4GB, max_gpu_util:60, max_mem_cpy_util:34, cyclic:True, process_cnt:2, processes:[{pid:25678, cur_mem_used:3GB},{pid:67234, cur_mem_used:1GB}]}                                 
        }
     """
-    ret_dict = dict()
+    node_dict = dict()
+    pod_dict = dict()
     promi = PrometheusConnect(url=url, disable_ssl=True)
     # except connection error
     try:
@@ -82,14 +147,16 @@ def profiling(url, pod_ip, ana_window='2m', metrics=MEM_UTIL):
                                               end_time=end_time)
     # reorganize data to label_config and metric_values
     metric_object_list = MetricsList(metric_data)
-    ret_dict = dict()
     for item in metric_object_list: # iterate through all the gpus on the node
         if 'gpu' not in item.label_config: # handle metric config info exception
             continue
         id = item.label_config['gpu']  # predefined key from dcgm (gpu index)
+        cur_usage = collect_cur_usage(int(id)) # nvml access GPU usage
+        if cur_usage['process_cnt'] > 0:
+            collect_pod_metrics(cur_usage['pid-mem'], node_name, id, pod_dict) # a pod may use multiple GPUs, so the dictionary value is appended
         # ip = item.label_config['instance']
         key = DOMAIN + "/gpu-" + id
-        cur_usage = collect_cur_usage(int(id))
+        # analyze mem util curve
         ts = item.metric_values.iloc[:, 1]  # metrics_values are two row df, 1st is timestamp, 2nd is value
         cur_usage['cyclic_pattern'] = False
         if ts.max() > 0:
@@ -99,8 +166,10 @@ def profiling(url, pod_ip, ana_window='2m', metrics=MEM_UTIL):
                 cur_usage['period'] = str(period)       
         cur_usage['max_mem_util'] = str(ts.max())
         # Important: flatten nested dictionary to string, otherwise error "cannot unmarshal string into Go value of type map[string]interface {}""
-        ret_dict[key] = str(cur_usage)
-    return ret_dict
+        #node_dict[key] = str(cur_usage)
+        # move the string cast to patch_annotation function
+        node_dict[key] = cur_usage
+    return node_dict, pod_dict
 
 def load_config():
     config_dict = dict()
@@ -154,58 +223,44 @@ def collect_gpu_attributes():
     attributes['mem_size'] = str(math.ceil(nvmlDeviceGetMemoryInfo(handle).total/pow(1024,3))) + 'GB'
     attributes['pcie_gen_width'] = str(nvmlDeviceGetCurrPcieLinkGeneration(handle)) + 'x' + str(nvmlDeviceGetCurrPcieLinkWidth(handle))
     key = DOMAIN + "/gpu-static" 
-    annotation = {key:str(attributes)}
+    annotation = {key:attributes}
     
     return annotation
 
 def app_top():
-    current_annotation = dict()
-    logging.info("profiler initialization")
+    node_ann_cur = dict()
+    pods_ann_cur = dict() # save this for annotation deletion when process ends
+    logging.info("Profiler initialization")
     config_dict = load_config()
     # add gpu static attributes
     gpu_attributes = collect_gpu_attributes()
     patch_annotation(config_dict['node_name'], gpu_attributes)
+    logging.info("Init add gpu static attributes \n{}".format(gpu_attributes))
+    # Init: remove pod annotatio if there is any, POD_KEY1:None
+    init_remove_annotation(config_dict['node_name'], POD_KEY1)
     while True:
         # profiling, add gpu dynamic status
-        new_annotation = profiling(url=config_dict['url'], pod_ip=config_dict['pod_ip'])
-        # update annotation if changes detected
-        if new_annotation != current_annotation:
-            patch_annotation(config_dict['node_name'], new_annotation)
-            current_annotation = new_annotation
+        node_ann_new, pods_ann_new = profiling(config_dict['url'], config_dict['pod_ip'],config_dict['node_name'])
+        # update node annotation if changes detected
+        if node_ann_new != node_ann_cur:
+            patch_annotation(config_dict['node_name'], node_ann_new)
+            logging.info("Node change detected, update node's GPU utilization \nnew:{}\nold:{}".format(node_ann_new, node_ann_cur))
+            node_ann_cur = node_ann_new
+        # update pod annotation
+        if pods_ann_new != pods_ann_cur:
+            logging.info("Pod change deteacted, update pods GPU utilization \nnew:{}\nold:{}".format(pods_ann_new,pods_ann_cur))
+            for name_ns, values in pods_ann_new.items(): # iterate all the pods needs to be annotated
+                pod_name, namespace = name_ns.split(":")
+                patch_annotation(pod_name, values, namespace) 
+            for name_ns, values in pods_ann_cur.items():
+                if name_ns not in pods_ann_new: # ended pods or processes
+                    pod_name, namespace = name_ns.split(":")
+                    logging.info("Remove pod {} annotation for finished process \n".format(pod_name))
+                    patch_annotation(pod_name, {POD_KEY1:None}, namespace) # tmp use POD_KEY1
+            pods_ann_cur = pods_ann_new
         time.sleep(30)
-
-
-def delete_annotations():
-    """assign key value to None for deletion, not in use in the main flow
-    """
-    #key = "GPU-0-DCGM_FI_DEV_MEM_COPY_UTIL"
-    delete_dict = dict()
-    node_name = ""
-    for m in ["job-type"]:
-        for i in range(8):
-            key = DOMAIN +"/GPU-" + str(i) + "-" + m
-            logging.debug(key)
-            delete_dict[key] = None 
-    if 'KUBERNETES_PORT' in os.environ:
-        config.load_incluster_config()
-    else:
-        logging.error("RUNNING cluster is not avaliable")
-        exit(1)
-    if "MY_NODE_NAME" in os.environ:
-        node_name = os.environ['MY_NODE_NAME']
-    else:
-        logging.error("MY_HOST_NAME cannot be found in environment variables, Please check profiler deployment file to include it as env.")
-        exit(1)
-    # assign key value to None for deletion
-    # body = {'metadata':{'annotations':{key:None}}}
-    body = {'metadata':{'annotations': delete_dict}}
-    v1 = client.CoreV1Api()
-    ret = v1.patch_node(node_name,body)
-    logging.info("delete node annotation {}, return {}, return type {}".format(key, ret, type(ret)))
-    return True
 
 
 if __name__ == '__main__':
     logging.basicConfig(format='%(asctime)s [%(levelname)s]: %(message)s',level=logging.INFO)
-    #delete_annotations()
     app_top()
