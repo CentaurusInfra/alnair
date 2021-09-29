d5450342ca1f33528e8a5a7e49c81fefc5cad026
yaohui <dingyaohui2016@gmail.com>
2021-09-29 11:04:59 -0700

    change path of main.go

--- autonomous-scheduler/UtilSched/cmd/scheduler/main.go
+++ autonomous-scheduler/UtilSched/cmd/scheduler/main.go
@@ -7,12 +7,16 @@ import (
 	"time"
 
     "github.com/YHDING23/AI-SIG/autonomous-scheduler/UtilSched/pkg/plugins/register"
+    "k8s.io/component-base/logs"
+
 )
 
 func main() {
 	rand.Seed(time.Now().UTC().UnixNano())
 
-	cmd := plugins.Register()
+	cmd := register.Register()
+	logs.InitLogs()
+	defer logs.FlushLogs()
 	if err := cmd.Execute(); err != nil {
 		_, _ = fmt.Fprintf(os.Stderr, "%v\n", err)
 		os.Exit(1)
